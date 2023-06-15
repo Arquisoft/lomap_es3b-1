6cf1ab5a333f17bfebc27b776f62697c7fd3d2e0
UO282631 <UO282631@uniovi.es>
2023-06-15 14:57:22 +0200

    Cambiada configuración pruebas

--- webapp/src/pages/maps/MapsPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import MapsPage from './MapsPage';
-
-describe('MapsPage', () => {
-    test('renders without error', () => {
-        render(<MapsPage/>);
-    });
-
-});
