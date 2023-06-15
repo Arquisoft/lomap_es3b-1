d6309587d9d02d0f98c8ed610dfe66c8e8b1f44f
UO282631 <UO282631@uniovi.es>
2023-06-15 22:56:57 +0200

    Arreglos de bugs de MinimumDistanceSlider

--- webapp/src/pages/maps/components/MinimumDistanceSlider.tsx
+++ webapp/src/pages/maps/components/MinimumDistanceSlider.tsx
@@ -19,8 +19,11 @@ export default function MinimumDistanceSlider(props: MinimumDistanceSliderProps)
     const [value2, setValue2] = React.useState([0, 30]);
 
     // Función que se ejecuta cada vez que cambia el valor del slider
-    const handleChange2 = async (event: any, newValue: number | number[], activeThumb: any) => {
-        handleChange2Aux(event, newValue, activeThumb, minDistance, setValue2,props).catch();
+    const handleChange2 = async (event: any, newValue: number | number[], activeThumb: any): Promise<void> => {
+        await handleChange2Aux(event, newValue, activeThumb, minDistance, setValue2, props)
+            .catch((error: Error) => {
+                // Manejar el error aquí si es necesario
+            });
     };
 
     // Renderizar el slider
