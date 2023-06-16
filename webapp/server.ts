099d10abdb238b6baf22e042a8b21c6ca391c74b
Luis Manuel Solares García <113901106+UO282631@users.noreply.github.com>
2023-06-16 23:03:19 +0200

    Cambios en server.ts para despliegue

--- webapp/server.ts
+++ webapp/server.ts
@@ -1,23 +1,18 @@
 import express,{Application} from 'express';
-import https from "https";
-import fs from "fs";
+import path from 'path';
 //for using an import here we need to configure the tsconfig.json
 //setting the option module to commonjs
-
-require ('dotenv').config();
 
 var app: Application = express()
 const port: number = 3000;
 
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-const options = {
-    key: fs.readFileSync('./certificate/private-key.pkey'),
-    cert: fs.readFileSync('./certificate/certificate.cert')
-}
-
-https.createServer(options,app).listen(port,():void=>{
-    console.log('(HTTPS) Webapp started on port '+ port);
+app.listen(port, ():void => {
+    console.log('Webapp started on port '+ port);
 }).on("error",(error:Error)=>{
     console.error('Error occured: ' + error.message);
 });
