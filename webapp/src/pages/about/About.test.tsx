32dcfde4590e6edcc4248a6f952e6d17c972ea22
UO282631 <UO282631@uniovi.es>
2023-06-23 19:40:53 +0200

    Añadido about y test

--- webapp/src/pages/about/About.test.tsx
+++ webapp/src/pages/about/About.test.tsx
@@ -9,6 +9,7 @@ describe('AboutPage', () => {
         expect(screen.getByText('¿Qué es LOMAP?')).toBeInTheDocument();
         expect(screen.getByText('¿Quíenes somos?')).toBeInTheDocument();
 
+
         expect(screen.getByText('LoMap es un sistema de desarrollo de software versátil y personalizable que permite a los ciudadanos crear mapas personalizados, almacenar información sobre lugares de interés y compartir experiencias con amigos. Su objetivo es facilitar la exploración de la ciudad y brindar a los usuarios una herramienta poderosa para descubrir nuevos lugares y establecimientos locales.')).toBeInTheDocument();
         expect(screen.getByText('Este trabajo ha sido realizado a partir del proyecto realizado por el grupo')).toBeInTheDocument();
         expect(screen.getByText('Lomap_es3b')).toHaveAttribute('href', 'https://github.com/Arquisoft/lomap_es3b');
