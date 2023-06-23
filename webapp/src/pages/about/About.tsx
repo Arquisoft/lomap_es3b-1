b7e78e591e1f56cd0eb4c1673d0f4d571e6e46d7
UO282631 <UO282631@uniovi.es>
2023-06-23 18:52:03 +0200

    Añadida página about sin contenido

---
+++ webapp/src/pages/about/About.tsx
@@ -0,0 +1,18 @@
+import "./About.css"
+
+function HelpPage(): JSX.Element {
+
+    return (
+        <>
+            <div className="aboutPage">
+                <div className="contenidoAbout">
+                    <div className="centroAbout">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default HelpPage;
